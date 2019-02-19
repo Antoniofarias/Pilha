cb8fe55d0161d9863a0b7604b0f80cd89d04f15c
Antonio <antoniofjj@hotmail.com>
2019-02-19 17:24:56 -0300

    Commit Pilha

--- Pilha/Program.cs
+++ Pilha/Program.cs
@@ -10,6 +10,22 @@ namespace Pilha
     {
         static void Main(string[] args)
         {
+            // Pilha - Primeira que entra é a última que sai
+
+            Stack<string> minhaPilha = new Stack<string>();
+
+            minhaPilha.Push("Primeira");
+            minhaPilha.Push("Segunda");
+            minhaPilha.Push("Terceira");
+            minhaPilha.Push("Quarta");
+            minhaPilha.Push("Quinta");
+
+            foreach (string carta in minhaPilha)
+            {
+                Console.WriteLine(carta);
+            }
+            Console.ReadKey();
         }
     }
 }
+// minhaPilha.Pop(); Remove o primeiro elemento da pilha
